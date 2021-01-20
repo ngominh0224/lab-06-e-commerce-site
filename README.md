e8887f3e99e055808f6d53838d2ab1e11a520e6c
Minh Ngo <ngo.minh0224@gmail.com>
2021-01-19 23:38:03 -0800

    added products

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # lab-06-e-commerce-site
+Choose a product to create a page:
+
+1) Create a products folder that contains:
+    -app.js
+    -data.js
+    -index.html
+    -render-chocolate.js
+2) Create a home page with HTML
+3) CSS and Product CSS page
+4) Import function example test js
---
+++ products/data.js
@@ -0,0 +1,38 @@
+export const chocolate = [
+    {
+        id: 1,
+        name: 'Kit-Kat',
+        image: '../assets/kitkat-reg.jpg',
+        description: 'Milk Chocolate Wafers',
+        category: 'candy',
+        size: 'regular',
+        price: 1.79
+    },
+    {
+        id: 2,
+        name: 'King-Size-Kit-Kat',
+        image: '../assets/kitkat-king.jpg',
+        description: 'Milk Chocolate Wafers',
+        category: 'candy',
+        size: 'king',
+        price: 2.19
+    },
+    {
+        id: 3,
+        name: 'Dark-Kit-Kat',
+        image: '../assets/darkkitkat-reg.jpg',
+        description: 'Dark Chocolate Wafers',
+        category: 'candy',
+        size: 'regular',
+        price: 1.79
+    },
+    {
+        id: 1,
+        name: 'King-Size-Dark-Kit-Kat',
+        image: '../assets/darkkitkat-king.jpg',
+        description: 'Dark Chocolate Wafers',
+        category: 'candy',
+        size: 'king',
+        price: 2.19
+    },
+];
