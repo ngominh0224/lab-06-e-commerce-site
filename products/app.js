783278f1ce5c5198aaca89d119f31e815dd1ed9b
Minh Ngo <ngo.minh0224@gmail.com>
2021-01-21 09:19:19 -0800

    changes to test

--- products/app.js
+++ products/app.js
@@ -1,4 +1,4 @@
 import { chocolate } from './data.js';
-console.log(chocolate);
+//console.log(chocolate);
 
-const list = document.getElementById('list');
+//const list = document.getElementById('list');
--- test/example.test.js
+++ test/example.test.js
@@ -6,14 +6,7 @@ import { renderChocolate } from '../products/render-chocolate.js';
 const test = QUnit.test;
 
 test('should take in a chocolate item and return li', (expect) => {
-    const expected = `<li class="list-item">
-<img class="image" src="../assets/kitkat-reg.jpg" height="500" width="500">
-<h2 class="name">Kit-Kat</h2>
-<p class="price">1.79</p>
-<p class="size">Regular</p>
-<p class="description">Milk Chocolate Wafers</p>
-<button class="button">Add To Cart</button>
-</li>`;
+    const expected = `<li class="list-item"><img class="image" src="../assets/kitkat-reg.jpg" height="500" width="500"><h2 class="name">Kit-Kat</h2><p class="price">1.79</p><p class="size">Regular</p><p class="description">Milk Chocolate Wafers</p><button class="button">Add To Cart</button></li>`;
 
     const actual = renderChocolate(chocolate[0]);
 
